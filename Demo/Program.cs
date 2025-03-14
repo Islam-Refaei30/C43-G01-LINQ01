f575b409fa798b022e19bfec3932fdd0d15e636c
islam-refaei <i.refaei.refaei.saadawi@gmail.com>
2025-03-15 01:36:41 +0200

    Part 07 Filtration [Restrication] Operators - Where

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -68,11 +68,46 @@ namespace Demo
 			//	Console.WriteLine(N);
 			//}
 			#endregion
+			#endregion
 
 			#region Part 06 DataSetup
-			Console.WriteLine(ProductList[0]);
-			Console.WriteLine(CustomerList[0]);
+			//Console.WriteLine(ProductList[0]);
+			//Console.WriteLine(CustomerList[0]);
 			#endregion
+
+			#region Part 07 Filtration [Restrication] Operators - Where
+			#region Get products out of stock
+			//var result = ProductList.Where(p => p.UnitsInStock == 0);
+			//result = from p in ProductList
+			//		 where p.UnitsInStock == 0
+			//		 select p;
+			//foreach (var item in result)
+			//{
+			//	Console.WriteLine(item);
+			//}
+			#endregion
+
+			#region Get products in stock and in category of Meat/Poultry 
+			//var result = ProductList.Where(P => P.UnitsInStock > 0 && P.Category == "Meat/Poultry");
+			//result = from p in ProductList
+			//		 where p.UnitsInStock > 0 && p.Category == "Meat/Poultry"
+			//		 select p;
+			//foreach (var item in result)
+			//{
+			//	Console.WriteLine(item);
+			//}
+			#endregion
+
+			#region Get products out of stock in first 10 elements
+			////Indexed Wheer
+			//// Valid only with fluent Syntex, can not be written in Query Syntax
+			//var result = ProductList.Where((P, I) => I < 10 && P.UnitsInStock > 0);
+			//foreach (var item in result)
+			//{
+			//	Console.WriteLine(item);
+			//}
+			#endregion
+
 			#endregion
 		}
 	}
