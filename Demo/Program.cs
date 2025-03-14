4c4a37ed768639c0b2b59ed19c7e6f8f460596e7
islam-refaei <i.refaei.refaei.saadawi@gmail.com>
2025-03-15 00:35:34 +0200

    Part 05 Linq Execution Ways

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -17,15 +17,15 @@
 			#endregion
 
 			#region Part 04 Linq Syntax
-			List<int> Numbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 7, 5, 4 };
+			//List<int> Numbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 7, 5, 4 };
 			#region Fluent Syntax
-			
-			// 1-call "LinQ Operator" as static method throgh (Enumrable class)
-			List<int> OddNumbers = Enumerable.Where(Numbers, (N) => N % 2 == 1).ToList();
+
+			//// 1-call "LinQ Operator" as static method throgh (Enumrable class)
+			//List<int> OddNumbers = Enumerable.Where(Numbers, (N) => N % 2 == 1).ToList();
 
 
-			// 2-call "LinQ Operator" as extention method
-			List<int> OddNumbers2 = Numbers.Where((N) => N % 2 == 1).ToList();
+			//// 2-call "LinQ Operator" as extention method
+			//List<int> OddNumbers2 = Numbers.Where((N) => N % 2 == 1).ToList();
 
 
 			#endregion
@@ -36,15 +36,38 @@
 			 *From Numbers N
 			 *Where N%2==1
 			 */
-			var OddNumbers3 = from N in Numbers
-							  where N % 2 == 1
-							  select N;
+			//var OddNumbers3 = from N in Numbers
+			//				  where N % 2 == 1
+			//				  select N;
 
 			#endregion
-			foreach (int n in OddNumbers3)
+			//foreach (int n in OddNumbers3)
+			//{
+			//	Console.WriteLine(n);
+			//}
+			#endregion
+
+			#region Part 05 Linq Execution Ways
+			#region Deferred Execution
+			//List<int> Numbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 7, 5, 4 };
+			//var Result = Numbers.Where((N => N % 2 == 1)); //Where is Deferred Execution
+			//Numbers.AddRange(new int[] { 11, 12, 13, 14, 15, 16, 17, 18 });
+			//foreach (int N in Result)
+			//{
+			//	Console.WriteLine(N);
+			//} 
+			#endregion
+
+			#region Immediate Execution
+			List<int> Numbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 7, 5, 4 };
+			var Result = Numbers.Where((N => N % 2 == 1)).ToList(); //Casting (ToList) is Immediate Execution
+			Numbers.AddRange(new int[] { 11, 12, 13, 14, 15, 16, 17, 18 });
+			foreach (int N in Result)
 			{
-				Console.WriteLine(n);
+				Console.WriteLine(N);
 			}
+			#endregion
+
 			#endregion
 		}
 	}
