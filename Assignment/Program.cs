107544fa1c2a19f75cb4a5331a0a7cc81ca0ad61
islam-refaei <i.refaei.refaei.saadawi@gmail.com>
2025-03-14 22:38:42 +0200

    Add project files.

---
+++ Assignment/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignment
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("test Push Assignment");
+		}
+	}
+}
---
+++ Demo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("test Push Demo");
+        }
+    }
+}
