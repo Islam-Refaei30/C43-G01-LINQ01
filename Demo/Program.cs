cbb7f09776a809be78e8477251062c0a5ea1eab6
islam-refaei <i.refaei.refaei.saadawi@gmail.com>
2025-03-14 23:10:06 +0200

    Part 03 What Is Linq

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("test Push Demo");
+			#region Part 03 What Is Linq
+			Console.WriteLine("test Push Demo");
+
+            List<int> numbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 7, 5, 4 };
+            List<int> Odd = numbers.Where((N) => N % 2 == 1).ToList(); //LinQ Orerator
+
+            foreach (int n in Odd)
+            {
+                Console.WriteLine(n);
+            } 
+            #endregion
         }
     }
 }
