b13023e5acc78a7a5c23058e678c4eae52e2ba8e
islam-refaei <i.refaei.refaei.saadawi@gmail.com>
2025-03-15 22:53:28 +0200

    LINQ - Restriction Operators

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,10 +1,50 @@
-﻿namespace Assignment
+﻿using static Assignment.ListGenerator;
+namespace Assignment
 {
 	internal class Program
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("test Push Assignment");
+			//Console.WriteLine("test Push Assignment");
+			//Console.WriteLine(ListGenerator.ProductList[0]);
+			//Console.WriteLine(ListGenerator.CustomerList[0]);
+			#region LINQ - Restriction Operators
+
+			#region 1. Find all products that are out of stock.
+			////fluent syntax
+			//var result = ProductList.Where(P => P.UnitsInStock < 1);
+
+			////Query Syntax
+			//result = from P in ProductList
+			//		 where P.UnitsInStock < 1
+			//		 select P;
+			#endregion
+			#region 2. Find all products that are in stock and cost more than 3.00 per unit
+			////Fluent Syntax
+			//var result = ProductList.Where(P => P.UnitsInStock > 0 && P.UnitPrice > 3.00M);
+
+			////Query Syntax
+			//result = from P in ProductList
+			//		 where P.UnitsInStock > 0 && P.UnitPrice > 3.00M
+			//		 select P;
+			#endregion
+			#region 3. Returns digits whose name is shorter than their value.
+			//Fluent Syntax
+			var result = ProductList.Where(P => P.ProductName.Length < P.ProductID);
+
+			//Query Syntax
+			result = from P in ProductList
+					 where P.ProductName.Length < P.ProductID
+					 select P;
+			#endregion
+
+			#endregion
+
+
+			foreach (var unit in result)
+			{
+				Console.WriteLine(unit);
+			}
 		}
 	}
 }
