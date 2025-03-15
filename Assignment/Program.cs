c9281c3b6f12919200141dbacec9b8def5e49fd3
islam-refaei <i.refaei.refaei.saadawi@gmail.com>
2025-03-16 00:20:36 +0200

    LINQ – Transformation Operators

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -108,30 +108,124 @@ namespace Assignment
 			//}
 			#endregion
 			#region 8. Create a list of all digits in the array whose second letter is 'i' that is reversed from the order in the original array.
-			string[] Arr = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+			//string[] Arr = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
 
-			var filteredReversedArr = Arr
-				.Where(word => word.Length > 1 && word[1] == 'i')
-				.Reverse();
-
-
-			 filteredReversedArr = (from word in Arr
-									where word.Length > 1 && word[1] == 'i'
-									select word).Reverse();
-
-			foreach (var word in filteredReversedArr)
-			{
-				Console.WriteLine(word);
-			}
-
-			#endregion
-			#endregion
+			//var filteredReversedArr = Arr
+			//	.Where(word => word.Length > 1 && word[1] == 'i')
+			//	.Reverse();
 
 
-			//foreach (var unit in result)
+			// filteredReversedArr = (from word in Arr
+			//						where word.Length > 1 && word[1] == 'i'
+			//						select word).Reverse();
+
+			//foreach (var word in filteredReversedArr)
 			//{
-			//	Console.WriteLine(unit);
+			//	Console.WriteLine(word);
 			//}
+
+			#endregion
+			#endregion
+
+			#region LINQ – Transformation Operators
+			#region 1. Return a sequence of just the names of a list of products.
+			////
+			//var result = ProductList.Select(P => new
+			//{
+			//	P.ProductName
+			//});
+
+			////
+			//result = from P in ProductList
+			//		 select new { P.ProductName };
+			#endregion
+			#region 2. Produce a sequence of the uppercase and lowercase versions of each word in the original array (Anonymous Types).
+			//string[] words = { "aPPLE", "BlUeBeRrY", "cHeRry" };
+			//var result = words.Select(word => new
+			//{
+			//	Upper = word.ToUpper(),
+			//	Lower = word.ToLower()
+			//});
+
+
+			//result = from word in words
+			//		select new
+			//		{
+			//		 Upper = word.ToUpper(),
+			//		 Lower = word.ToLower()
+			//		};
+
+			//foreach (var item in result)
+			//{
+			//	Console.WriteLine(item.Upper);
+			//	Console.WriteLine(item.Lower);
+			//}
+			#endregion
+			#region 3. Produce a sequence containing some properties of Products, including UnitPrice which is renamed to Price in the resulting type.
+			////
+			//var result = ProductList.Select(P => new
+			//{
+			//	ID = P.ProductID,
+			//	Price = P.UnitPrice,
+			//	Name = P.ProductName
+			//});
+
+			////
+			//result = from P in ProductList
+			//		 select new 
+			//		 {
+			//			 ID = P.ProductID,
+			//			 Price = P.UnitPrice,
+			//			 Name = P.ProductName
+			//		 };
+			#endregion
+			#region 4. Determine if the value of ints in an array match their position in the array.
+			//int[] Arr = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+
+			////var result = Arr.Select((Value, Index) => new { Value, Index }).Where(item => item.Value == item.Index);
+			//var result = Arr.Select((value, index) => new { Number = value, InPlace = value == index });
+
+			#endregion
+			#region 5. Returns all pairs of numbers from both arrays such that the number from numbersA is less than the number from numbersB.
+			//int[] numbersA = { 0, 2, 4, 5, 6, 8, 9 };
+			//int[] numbersB = { 1, 3, 5, 7, 8 };
+
+			//var result = numbersA.SelectMany(a => numbersB.Where(b => a < b), (a, b) => $"{a} is less than {b}");
+
+
+			//result = from a in numbersA
+			//		 from b in numbersB
+			//		 where a < b
+			//		 select $"{a} is less than {b}";
+			#endregion
+			#region 6. Select all orders where the order total is less than 500.00.
+			////
+			//var result = CustomerList.SelectMany(O => O.Orders).Where(T => T.Total < 500.00M);
+
+			////
+			//result = from O in CustomerList
+			//		 from T in O.Orders
+			//		 where T.Total < 500.00M
+			//		 select T;
+			#endregion
+			#region 7. Select all orders where the order was made in 1998 or later.
+			//
+			var result = CustomerList.SelectMany(O => O.Orders).Where(T => T.OrderDate.Year >= 1998);
+
+			//
+			result = from O in CustomerList
+					 from T in O.Orders
+					 where T.OrderDate.Year >= 1998
+					 select T;
+			#endregion
+
+			#endregion
+
+
+			foreach (var item in result)
+			{
+				Console.WriteLine(item);
+			}
 		}
 	}
 }
