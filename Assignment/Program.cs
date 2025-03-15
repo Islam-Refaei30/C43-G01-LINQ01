633767e1f75213e8ac9818cb4a43367fff8f240f
islam-refaei <i.refaei.refaei.saadawi@gmail.com>
2025-03-15 23:42:48 +0200

    LINQ - Ordering Operators

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -29,22 +29,109 @@ namespace Assignment
 			//		 select P;
 			#endregion
 			#region 3. Returns digits whose name is shorter than their value.
-			//Fluent Syntax
-			var result = ProductList.Where(P => P.ProductName.Length < P.ProductID);
+			//string[] Arr = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
 
-			//Query Syntax
-			result = from P in ProductList
-					 where P.ProductName.Length < P.ProductID
-					 select P;
+			//var result = Arr.Where((word, index) => word.Length < index);
+
+			//foreach (var word in result)
+			//{
+			//	Console.WriteLine(word);
+			//}
 			#endregion
 
 			#endregion
 
+			#region LINQ - Ordering Operators
+			#region 1. Sort a list of products by name
+			////Fluent Syntax
+			//var result = ProductList.OrderBy(P => P.ProductName);
 
-			foreach (var unit in result)
+			////Query Syntax
+			//result = from P in ProductList
+			//		 orderby P.ProductName
+			//		 select P;
+			#endregion
+			#region 2. Uses a custom comparer to do a case-insensitive sort of the words in an array.
+			//string[] Arr = { "aPPLE", "AbAcUs", "bRaNcH", "BlUeBeRrY", "ClOvEr", "cHeRry" };
+
+			//var sortedArr = Arr.OrderBy(word => word, StringComparer.OrdinalIgnoreCase);
+
+			//foreach (var word in sortedArr)
+			//{
+			//	Console.WriteLine(word);
+			//}
+			#endregion
+			#region 3. Sort a list of products by units in stock from highest to lowest.
+			////Fluent Syntax
+			//var result = ProductList.OrderByDescending(P => P.UnitsInStock);
+
+			////Query Syntax
+			//result = from P in ProductList
+			//		 orderby P.UnitsInStock descending
+			//		 select P;
+			#endregion
+			#region 4. Sort a list of digits, first by length of their name, and then alphabetically by the name itself.
+			//string[] Arr = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+
+			//var sortedArr = Arr.OrderBy(N => N.Length).ThenBy(N => N);
+			//foreach (var word in sortedArr)
+			//{
+			//	Console.WriteLine(word);
+			//}
+			#endregion
+			#region 5. Sort first by word length and then by a case-insensitive sort of the words in an array.
+			//string[] words = { "aPPLE", "AbAcUs", "bRaNcH", "BlUeBeRrY", "ClOvEr", "cHeRry" };
+
+			//var SortedWords = words.OrderBy(W => W.Length).ThenBy(W => W, StringComparer.OrdinalIgnoreCase);
+
+			//foreach (var word in SortedWords)
+			//{
+			//	Console.WriteLine(word);
+			//}
+			#endregion
+			#region 6. Sort a list of products, first by category, and then by unit price, from highest to lowest.
+			////Fluent Syntax
+			//var result = ProductList.OrderByDescending(P => P.Category).ThenBy(P => P.UnitPrice);
+
+			////Query Syntaxz
+			//result = from p in ProductList
+			//		 orderby p.Category, p.UnitPrice descending
+			//		 select p;
+			#endregion
+			#region 7. Sort first by word length and then by a case-insensitive descending sort of the words in an array.
+			//string[] Arr = { "aPPLE", "AbAcUs", "bRaNcH", "BlUeBeRrY", "ClOvEr", "cHeRry" };
+			//var SortedArr = Arr.OrderBy(W => W.Length).ThenByDescending(W => W, StringComparer.OrdinalIgnoreCase);
+
+			//foreach (var word in SortedArr)
+			//{
+			//	Console.WriteLine(word);
+			//}
+			#endregion
+			#region 8. Create a list of all digits in the array whose second letter is 'i' that is reversed from the order in the original array.
+			string[] Arr = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+
+			var filteredReversedArr = Arr
+				.Where(word => word.Length > 1 && word[1] == 'i')
+				.Reverse();
+
+
+			 filteredReversedArr = (from word in Arr
+									where word.Length > 1 && word[1] == 'i'
+									select word).Reverse();
+
+			foreach (var word in filteredReversedArr)
 			{
-				Console.WriteLine(unit);
+				Console.WriteLine(word);
 			}
+
+			#endregion
+			#endregion
+
+
+			//foreach (var unit in result)
+			//{
+			//	Console.WriteLine(unit);
+			//}
 		}
 	}
 }
